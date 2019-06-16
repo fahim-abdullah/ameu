92e346ceeb6903cbee66fc292b56e67f800e9b34
fahim-abdullah <fahimabdullahh@gmail.com>
2019-06-17 02:26:00 +0800

    members set up

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,4 +1,54 @@
 class MembersController < ApplicationController
+	before_action :set_timezone
+
 	def index
+	end
+
+	def all
+	end
+
+	def new 
+		@member = Member.new
+	end
+
+	def create
+		@member = Member.new(member_params)
+		if @member.save
+			redirect_to new_member_path
+		else
+			render 'new'
+		end
+	end
+
+	def show
+		@member = Member.find(params[:id])
+	end
+
+	def edit
+		@member = Member.find(params[:id])
+	end
+
+	def update 
+		@member = Member.find(params[:id])
+		if @member.update(member_params)
+			redirect_to all_members_path
+		else
+			render 'show'
+		end
+	end
+
+	def destroy
+		@member = Member.find(params[:id])
+		@member.destroy
+		# flash[:notice] = "Article was successfully deleted"
+		redirect_to all_members_path
+	end
+
+	private 
+	def set_timezone
+		Time.zone = "Kuala Lumpur"
+	end
+	def member_params 
+		params.require(:member).permit(:region, :office, :name, :designation, :facebook, :linkedin, :twitter, :ig, :email, :profpic)
 	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
 	root 'statics#home'
 	get 'about', to: 'statics#about'
 	get 'all_events', to: 'events#all_events'
+	get 'all_members', to: 'members#all_members'
 
 
 	resources :events
