c7f14e0eef076afcaa20da8f9d5f7a8b161c85e9
fahim-abdullah <fahimabdullahh@gmail.com>
2019-10-04 04:46:30 +0800

    user tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_16_184344) do
+ActiveRecord::Schema.define(version: 2019_10_03_204350) do
 
   create_table "dialogues", force: :cascade do |t|
     t.string "title"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2019_06_16_184344) do
     t.string "ig"
     t.string "email"
     t.string "profpic"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
